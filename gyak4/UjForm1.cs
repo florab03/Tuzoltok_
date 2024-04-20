b2fc752e6d7ae9257b730edd5253bb4a72809e7d
bflora03 <127041138+bflora03@users.noreply.github.com>
2024-04-20 21:51:53 +0200

    proba ujra

---
+++ gyak4/UjForm1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gyak4
+{
+    public partial class UjForm1 : Form
+    {
+        //próba
+        public UjForm1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ gyak4/UjForm2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gyak4
+{
+    public partial class UjForm2 : Form
+    {
+        public UjForm2()
+        {
+            InitializeComponent();
+        }
+    }
+}
